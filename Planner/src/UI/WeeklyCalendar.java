7705a2ddc0229979e3d3ef03aa1ac4890efa890c
Grace Fernandez <60633655+gracefernandez@users.noreply.github.com>
2021-02-19 23:46:02 -0400

    Init commit Proyect
    JFrame and calendar libs initialyze

---
+++ Planner/src/UI/WeeklyCalendar.java
@@ -0,0 +1,19 @@
+package UI;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class WeeklyCalendar extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public WeeklyCalendar() {
+		super();
+		this.setSize(500, 200);
+		this.setBackground(Color.BLACK);
+	}
+}
